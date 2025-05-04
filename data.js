8146bffd942abe64cea4cc3b370ab8fdda677664
nilla-moige <113868239+nilla-moige@users.noreply.github.com>
2025-05-03 20:30:47 -0400

    update locations

--- data.js
+++ data.js
@@ -12,20 +12,91 @@ const defaultLocations = [
       name: "Smokey Joe's",
       lat: 39.95474,
       lng: -75.20264,
-      image: null
+      image: "/images/smokes.jpeg"
     },
     {
       id: "houston",
       name: "Houston Market",
       lat: 39.95115,
       lng: -75.19383,
-      image: null
+      image: "images/houston.jpeg"
     },
     {
       id: "commons",
       name: "1920 Commons",
       lat: 39.95250,
       lng: -75.19920,
-      image: null
+      image: "/images/commons.jpg"
+    },
+    {
+      id: "sweetgreen",
+      name: "Sweetgreen",
+      lat: 39.95384,
+      lng: -75.19526,
+      image: "/images/sweetgreen.jpeg.webp"
+    },
+    {
+      id: "greeklady",
+      name: "Greek Lady",
+      lat: 39.95195,
+      lng: -75.19792,
+      image: "/images/greeklady.jpg"
+    },
+    {
+      id: "allegro",
+      name: "Allegro Pizza",
+      lat: 39.95402,
+      lng: -75.19930,
+      image: "/images/allegropizza.png.avif"
+    },
+    {
+      id: "hipcityveg",
+      name: "HipCityVeg",
+      lat: 39.95415,
+      lng: -75.19630,
+      image: "/images/hipcityveg.png"
+    },
+    {
+      id: "goldie",
+      name: "Goldie",
+      lat: 39.95267,
+      lng: -75.19619,
+      image: "/images/goldie.jpg"
+    },
+    {
+      id: "dimsumhouse",
+      name: "Dim Sum House",
+      lat: 39.95527,
+      lng: -75.19893,
+      image: "/images/dimsum.jpg"
+    },
+    {
+      id: "ramenbar",
+      name: "Ramen Bar",
+      lat: 39.95462,
+      lng: -75.20061,
+      image: "/images/ramen.jpeg"
+    },
+    {
+      id: "honeygrow",
+      name: "Honeygrow",
+      lat: 39.95443,
+      lng: -75.19549,
+      image: "/images/honeygrow.jpeg"
+    },
+    {
+      id: "sabrinas",
+      name: "Sabrina’s Café",
+      lat: 39.95601,
+      lng: -75.20202,
+      image: "/images/sabrinas.jpeg"
+    },
+    {
+      id: "handynasty",
+      name: "Han Dynasty",
+      lat: 39.95450,
+      lng: -75.20225,
+      image: "/images/handynasty.jpg"
     }
   ];
+  
